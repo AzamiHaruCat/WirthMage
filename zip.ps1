aa0a46f346731096a12c4244fae1d742218796fe
AzamiHaruCat <haruazamicat+git@gmail.com>
2025-07-11 01:18:13 +0900

    add

---
+++ zip.ps1
@@ -0,0 +1,16 @@
+﻿if ("$PWD" -ne "$PSScriptRoot") { exit 1 }
+
+$zipName = "WirthMage.zip"
+$baseDir = "WirthMage"
+$tempDir = Join-Path $Env:TEMP "WirthMage_$(New-Guid)"
+New-Item -ItemType Directory -Path "$tempDir\$baseDir" -Force | Out-Null
+Set-Location $tempDir
+
+Copy-Item "$PSScriptRoot\dist\WirthMage.exe" -Destination "$baseDir\WirthMage.exe"
+Copy-Item "$PSScriptRoot\LICENSE" -Destination "$baseDir\LICENSE.txt"
+Copy-Item "$PSScriptRoot\README.md" -Destination "$baseDir\README.txt"
+robocopy "$PSScriptRoot\lib" "$baseDir\lib" /E /NFL /NDL /NJH /NJS /NC /NS
+
+Compress-Archive -Path $baseDir -DestinationPath "$PSScriptRoot\dist\$zipName" -Force -CompressionLevel Optimal
+Set-Location $PSScriptRoot
+Remove-Item $tempDir -Force -Recurse
