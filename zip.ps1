e6ccc2aff78c3a3d1bdc2e0500a58643a277b720
AzamiHaruCat <haruazamicat+git@gmail.com>
2025-07-11 23:36:14 +0900

    fix

--- zip.ps1
+++ zip.ps1
@@ -9,6 +9,7 @@ Set-Location $tempDir
 Copy-Item "$PSScriptRoot\dist\WirthMage.exe" -Destination "$baseDir\WirthMage.exe"
 Copy-Item "$PSScriptRoot\LICENSE" -Destination "$baseDir\LICENSE.txt"
 Copy-Item "$PSScriptRoot\README.md" -Destination "$baseDir\README.txt"
+Copy-Item "$PSScriptRoot\ChangeLog.txt" -Destination "$baseDir\ChangeLog.txt"
 robocopy "$PSScriptRoot\lib" "$baseDir\lib" /E /NFL /NDL /NJH /NJS /NC /NS
 
 Compress-Archive -Path $baseDir -DestinationPath "$PSScriptRoot\dist\$zipName" -Force -CompressionLevel Optimal
