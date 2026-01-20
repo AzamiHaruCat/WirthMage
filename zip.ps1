58e151f71de31956504ddfccefd6dca585542d21
AzamiHaruCat <haruazamicat+git@gmail.com>
2026-01-20 16:07:36 +0900

    不要なexeを除外

--- zip.ps1
+++ zip.ps1
@@ -1,16 +1,33 @@
 ﻿if ("$PWD" -ne "$PSScriptRoot") { exit 1 }
 
 $zipName = "WirthMage.zip"
-$baseDir = "WirthMage"
+$baseDirName = "WirthMage"
 $tempDir = Join-Path $Env:TEMP "WirthMage_$(New-Guid)"
-New-Item -ItemType Directory -Path "$tempDir\$baseDir" -Force | Out-Null
+$distBaseDir = New-Item -ItemType Directory -Path "$tempDir\$baseDirName" -Force
 Set-Location $tempDir
 
-Copy-Item "$PSScriptRoot\dist\app.dist\*" -Destination "$baseDir" -Recurse
-Copy-Item "$PSScriptRoot\LICENSE" -Destination "$baseDir\LICENSE.txt"
-Copy-Item "$PSScriptRoot\README.md" -Destination "$baseDir\README.txt"
-Copy-Item "$PSScriptRoot\ChangeLog.txt" -Destination "$baseDir\ChangeLog.txt"
+Copy-Item "$PSScriptRoot\dist\app.dist\*" -Destination "$distBaseDir" -Recurse -Exclude @(
+	"compare.exe"
+	"composite.exe"
+	"conjure.exe"
+	"identify.exe"
+	"mogrify.exe"
+	"montage.exe"
+	"stream.exe"
+)
+Copy-Item "$PSScriptRoot\LICENSE" -Destination "$distBaseDir\LICENSE.txt"
+Copy-Item "$PSScriptRoot\README.md" -Destination "$distBaseDir\README.txt"
+Copy-Item "$PSScriptRoot\ChangeLog.txt" -Destination "$distBaseDir\ChangeLog.txt"
 
-Compress-Archive -Path $baseDir -DestinationPath "$PSScriptRoot\dist\$zipName" -Force -CompressionLevel Optimal
+Get-ChildItem $distBaseDir -Recurse -File | Sort-Object Directory, {
+	if ($_.Extension -eq ".exe") { 1 }
+	elseif ($_.Extension -in ".txt", ".md") { 2 }
+	else { 3 }
+}, Extension, Name | ForEach-Object {
+	"- " + ($_.FullName -replace "^$([regex]::Escape($distBaseDir.FullName))\\?", "")
+} | Out-File "$PSScriptRoot\dist\$zipName.files.txt" -Encoding utf8
+
+Compress-Archive -Path $distBaseDir -DestinationPath "$PSScriptRoot\dist\$zipName" -Force -CompressionLevel Optimal
+
 Set-Location $PSScriptRoot
 Remove-Item $tempDir -Force -Recurse
